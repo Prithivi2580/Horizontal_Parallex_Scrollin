e138a4cfe11013da8a69e1e52b0601d8a90e3c73
CodeSandbox <support@codesandbox.io>
2024-10-28 15:05:36 +0000

    Sandbox snapshot commit

--- main.js
+++ main.js
@@ -1,24 +1,101 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+gsap.registerPlugin(ScrollTrigger);
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", () => {
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+
+  const stickySection = document.querySelector(".sticky");
+  const stickyHeader = document.querySelector(".sticky-header");
+  const cards = document.querySelectorAll(".card");
+  console.log(cards);
+  const stickyHeight = window.innerHeight * 5;
+
+  const transforms = [
+    [
+      [10, 50, -10, 10],
+      [20, -10, -45, 20],
+    ],
+    [
+      [0, 47.5, -10, 15],
+      [-25, 15, -45, 30],
+    ],
+    [
+      [0, 52.5, -10, 5],
+      [15, -5, -40, 60],
+    ],
+    [
+      [0, 50, 30, -80],
+      [20, -10, 60, 5],
+    ],
+    [
+      [0, 55, -15, 30],
+      [25, -25, 60, 95],
+    ],
+  ];
+
+  ScrollTrigger.create({
+    trigger: stickySection,
+    start: "top top",
+    end: `+=${stickyHeight}px`,
+    pin: true,
+    pinSpacing: true,
+    markers: true,
+    onUpdate: (self) => {
+      const progress = self.progress;
+
+      const maxTranslate = stickyHeader.offsetWidth - window.innerWidth;
+      const translateX = -progress * maxTranslate;
+      gsap.set(stickyHeader, { x: translateX });
+
+      cards.forEach((card, index) => {
+        const delay = index * 0.1125;
+        const cardProgress = Math.max(0, Math.min((progress - delay) * 2, 1));
+
+        if (cardProgress > 0) {
+          const cardStartX = 25;
+          const cardEndX = -650;
+          const yPos = transforms[index][0];
+          const rotations = transforms[index][1];
+
+          const cardX = gsap.utils.interpolate(
+            cardStartX,
+            cardEndX,
+            cardProgress
+          );
+
+          const yProgress = cardProgress * 3;
+          const yIndex = Math.min(Math.floor(yProgress), yPos.length - 2);
+          const yInterpolation = yProgress - yIndex;
+          const cardY = gsap.utils.interpolate(
+            yPos[yIndex],
+            yPos[yIndex + 1],
+            yInterpolation
+          );
+
+          const cardRotation = gsap.utils.interpolate(
+            rotations[yIndex],
+            rotations[yIndex + 1],
+            yInterpolation
+          );
+
+          gsap.set(card, {
+            xPercent: cardX,
+            yPercent: cardY,
+            rotate: cardRotation,
+            opacity: 1,
+          });
+        } else {
+          gsap.set(card, { opacity: 0 });
+        }
+      });
+    },
+  });
+});
